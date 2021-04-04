14470083621412361bb0c4a56ea4ea620273026e
SurajLodh <surajlodh10962@gmail.com>
2021-04-04 21:00:04 +0530

    api and graph working

--- REST/views.py
+++ REST/views.py
@@ -16,29 +16,20 @@ from json import dumps
 class PersonView(APIView):
 
     def get(self, request, format=None):
-    #1 Data
-        # A = {"date" : 'January',"High" : 47,"Low" : 27}
-        # B = {"date" : 'Fabruary',"High" : 47,"Low" : 25}
-        # C = {"date" : 'March',"High" : 53,"Low" : 21}
-        
-        # Temp_Data = [A,B,C]
-        
-        # data = {"Temp_Data": Temp_Data}
-
-    #Implementing data
-        os.chdir(r"C:\Users\suraj\Downloads\pc documents")
+    
+        os.chdir(r"C:\Users\suraj\Downloads")
         os.getcwd()
         os.listdir()
         df = pd.read_csv('owid-covid-data.csv', encoding='utf-8', na_values=None, error_bad_lines=False)
-        df = df.sort_values(by='total_vaccinations', ascending=False)
+        df = df.sort_values(by='total_vaccinations', ascending=False).fillna(0)
 
-        df2 = df[['date', 'total_vaccinations','people_fully_vaccinated']][df['location'] == 'Asia']
+        df2 = df[['date', 'total_vaccinations','people_fully_vaccinated']][df['continent'] == 'Asia']
         df2 = df2[:30]
         df2 = df2.sort_values(by='total_vaccinations', ascending=True)
-        data = df2.to_json(df2)
-        
+                
         result=df2.to_json(orient='table')
         parsed_result=json.loads(result)
-        data = json.dumps(parsed_result['data'])
-
+        #data1 = json.dumps(parsed_result['data'])    #for user view during debugg
+        data2 = parsed_result['data']      
+        data = {'Asia':data2}
         return Response(data) 
